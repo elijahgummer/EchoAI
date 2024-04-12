a796a170dc22e59408ab82c889de04e3c0af4929
Elijah Gummer <elijahgummer5@gmail.com>
2024-04-12 16:23:38 +1000

    added different modes

--- numberGuess.py
+++ numberGuess.py
@@ -14,4 +14,15 @@ print("1 = Baby Mode")
 print("2 = Regular")
 print("3 = Hard")
 print("------------------------------------")
+mode = input(int("Please Enter Difficulty: "))
+
+##MODE SELECTION
+if mode == 1:
+    print("You Have Chosen Baby Mode!!!")
+elif mode == 2:
+    print("You Have Chosen Regular!!!")
+elif mode == 3: 
+    print("You Have Chosen Hard Mode!!!")
+else
+    print("Somthing Went Wrong...")
 
